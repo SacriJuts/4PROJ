b9fde61710d2704957118c876640e9805b57742f
Hugo <jutslegameur@gmail.com>
2020-06-10 16:06:12 +0200

    Fixed static includes

--- webserver/front/templates/front/commun/ouvrir.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	// Initialisation de la connexion
-	
-	$liaison = mysqli_connect("127.0.0.1","root","") or die ("Accès à la base de donnée impossible !!!");
-	mysqli_select_db($liaison, "formulaire_mail") or die ("Accès à la base de donnée impossible !!!");
-	
-?>
